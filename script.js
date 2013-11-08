25e6034a9cd928c1be5eb3c1041bbe82d5be9297
PCTIEEE <IEEEstudents@pct.edu>
2013-11-07 23:07:14 -0500

    v 0.0.03
    v 0.0.03
    
    - totalamount field is now one line
    - Colored the pay and cancel buttons
    - Added outset border to all buttons
    - Quantity and items can no longer stack on each other without prior
    input
    - Total Sales amount is now visible by clicking on "*" key
    - Live view of quantity now implemented

--- script.js
+++ script.js
@@ -3,10 +3,19 @@ var number; 		// Number Button
 var x="";			// Quantity Purchased
 var itemc;			// Selected item's cost
 var listbefore;
-var numlistbefore;
+var numlistbefore = "";
 var totallistbefore;
 var totalrow;
 var totalamount = 0;	// Total Purchase Amount
+
+var waitpur = 0;			// Wait variable for prior item input to be complete
+
+// Storage Variables
+
+var specialmenucon;		// Returns if special menu modifications are accepted
+var totalsales = 0;
+
+// End Storage Variables
 
 //Default Values
 //Item 1
@@ -39,87 +48,118 @@ var item5 = "Water";
 var item5c = 1;
 var item5t=item5+item5c;
 
+
+// Main Program Code
+
 function itemPress(item){
-	listbefore = document.getElementById("list").innerHTML;
-	switch (item)
-	{
-		case 0:
-			document.getElementById("list").innerHTML=listbefore + item0t + "<br />";
-			itemc = item0c;
-		break;
-		case 1:
-			document.getElementById("list").innerHTML=listbefore + item1t + "<br />";
-			itemc = item1c;
-		break;
-		case 2:
-			document.getElementById("list").innerHTML=listbefore + item2t + "<br />";
-			itemc = item2c;
-		break;
-		case 3:
-			document.getElementById("list").innerHTML=listbefore + item3t + "<br />";
-			itemc = item3c;
-		break;
-		case 4:
-			document.getElementById("list").innerHTML=listbefore + item4t + "<br />";
-			itemc = item4c;
-		break;
-		case 5:
-			document.getElementById("list").innerHTML=listbefore + item5t + "<br />";
-			itemc = item5c;
-		break;
+	if (waitpur == 0){
+		listbefore = document.getElementById("list").innerHTML;
+		switch (item)
+		{
+			case 0:
+				document.getElementById("list").innerHTML=listbefore + item0t + "<br />";
+				itemc = item0c;
+			break;
+			case 1:
+				document.getElementById("list").innerHTML=listbefore + item1t + "<br />";
+				itemc = item1c;
+			break;
+			case 2:
+				document.getElementById("list").innerHTML=listbefore + item2t + "<br />";
+				itemc = item2c;
+			break;
+			case 3:
+				document.getElementById("list").innerHTML=listbefore + item3t + "<br />";
+				itemc = item3c;
+			break;
+			case 4:
+				document.getElementById("list").innerHTML=listbefore + item4t + "<br />";
+				itemc = item4c;
+			break;
+			case 5:
+				document.getElementById("list").innerHTML=listbefore + item5t + "<br />";
+				itemc = item5c;
+			break;
+		}
+		waitpur = 1;
 	}
 }
 
 function numPress(number){
-	numlistbefore = document.getElementById("quantity").innerHTML;
-	totallistbefore = document.getElementById("total").innerHTML;
-	switch (number)
-	{
-		case 0:
-			x= x+"0";
-		break;
-		case 1:
-			x= x+"1";
-		break;
-		case 2:
-			x= x+"2";
-		break;
-		case 3:
-			x= x+"3";
-		break;
-		case 4:
-			x= x+"4";
-		break;
-		case 5:
-			x= x+"5";
-		break;
-		case 6:
-			x= x+"6";
-		break;
-		case 7:
-			x= x+"7";
-		break;
-		case 8:
-			x= x+"8";
-		break;
-		case 9:
-			x= x+"9";
-		break;
-		case 99:
-			totalrow =  x * itemc;
-			totalamount = totalamount + totalrow;
-			document.getElementById("quantity").innerHTML=numlistbefore + x + "<br />";
-			document.getElementById("total").innerHTML=totallistbefore + "$" + totalrow + "<br />";
-			document.getElementById("totalamount").innerHTML="$" + totalamount;
-			x="";
-		break;
+	if (waitpur == 1){
+		totallistbefore = document.getElementById("total").innerHTML;
+		switch (number)
+		{
+			case 0:
+				x= x+"0";
+			break;
+			case 1:
+				x= x+"1";
+			break;
+			case 2:
+				x= x+"2";
+			break;
+			case 3:
+				x= x+"3";
+			break;
+			case 4:
+				x= x+"4";
+			break;
+			case 5:
+				x= x+"5";
+			break;
+			case 6:
+				x= x+"6";
+			break;
+			case 7:
+				x= x+"7";
+			break;
+			case 8:
+				x= x+"8";
+			break;
+			case 9:
+				x= x+"9";
+			break;
+		}
+		document.getElementById("quantity").innerHTML=numlistbefore + x;
 	}
 }
 
-function cancel(){
-	document.getElementById("quantity").innerHTML="Quantity<br />";
-	document.getElementById("list").innerHTML="Product Price Per<br />";
-	document.getElementById("total").innerHTML="Total<br />";
-	document.getElementById("totalamount").innerHTML="<br />";
+function equalsign(){
+	//numlistbefore = document.getElementById("quantity").innerHTML;
+	if (waitpur == 1){
+		totalrow =  x * itemc;
+		totalamount = totalamount + totalrow;
+		numlistbefore = document.getElementById("quantity").innerHTML=  numlistbefore + x + "<br />";
+		document.getElementById("total").innerHTML=totallistbefore + "$" + totalrow + "<br />";
+		document.getElementById("totalamount").innerHTML="$" + totalamount;
+		waitpur = 0;
+		x="";
+	}
+}
+
+function paid(){
+	totalsales = totalsales + totalamount;
+	document.getElementById("quantity").innerHTML="";
+	document.getElementById("list").innerHTML="";
+	document.getElementById("total").innerHTML="$" + 0;
+	document.getElementById("totalamount").innerHTML="$" + 0;
+	waitpur = 0;
 	totalamount = 0;
+}
+
+function cancel(){
+	document.getElementById("quantity").innerHTML="";
+	document.getElementById("list").innerHTML="";
+	document.getElementById("total").innerHTML="$" + 0;
+	document.getElementById("totalamount").innerHTML="$" + 0;
+	waitpur = 0;
+	totalamount = 0;
+}
+
+
+// Storage/Inventory/Sales Code
+
+function specialmenuPress(){
+	specialmenucon = confirm("Total Sales: $" + totalsales + "\n\n" + "Inventory:");
 }
